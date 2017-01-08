e0f5a011a121c244690d5865ef21306e55713b0e
annecylee <annecylee@gmail.com>
2017-01-07 22:25:37 -0800

    Add API
    Add Google StreetView API and NY Times Article Search API

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 
+//Capitalize First Letter
+String.prototype.capitalizeFirstLetter = function() {
+    return this.replace(/\w\S*/g, function(txt){
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
 function loadData() {
 
     var $body = $('body');
@@ -12,8 +19,39 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var street = $('#street').val().capitalizeFirstLetter();
+    var city = $('#city').val().capitalizeFirstLetter();
+    var address = street + ', ' + city;
 
-    // YOUR CODE GOES HERE!
+    $greeting.text("So, you want to live at " + address + "?");
+    var addressUrl = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + address;
+    $body.append('<img class="bgimg" src="' + addressUrl + '">');
+
+    //load NY times
+    //NYT Article Search API
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "xxxxxxxxxx",
+      'q': city,
+      'sort': "newest"
+    });
+
+    //Load Jason-encoded data
+    $.getJSON(url, function(data){
+      $('#nytimes-header').text('New York Times Articles About ' + city)
+      var articles = data['response']['docs']
+      $.each(articles,function(index,value){
+        var webUrl = value['web_url'];
+        var leadP = value['lead_paragraph'];
+        var title = value['headline']['main']
+        if (webUrl !== null && leadP != null){
+          $("#nytimes-articles").append("<li class='article'><a href='" + webUrl + "'>" + title + "</a>" +
+          "<p>" + leadP + "</p></li>")
+        }
+      })
+    }).fail(function(){
+      $('#nytimes-header').text('New York Times Articles Could Not Be Loaded')
+    })
 
     return false;
 };
